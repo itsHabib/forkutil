9ab510945b870bc2ad2d2d162e1b761d340b49ad
itsHabib <michaelhabib1868@gmail.com>
2018-01-21 15:01:54 -0800

    implement search and api TODO fix structure to remove circular dependencies

--- api/api.go
+++ api/api.go
@@ -3,6 +3,7 @@ package api
 import (
 	"github.com/itsHabib/art"
 	"github.com/itsHabib/forkutil/fork"
+	"github.com/itsHabib/forkutil/search"
 	"github.com/spf13/viper"
 )
 
@@ -15,6 +16,7 @@ func GitHubAPI() *art.API {
 		token := viper.GetString("token")
 		api.SetAuth(art.NewAuthToken(token))
 		api.AddResource("fork", fork.GetForkResource())
+		api.AddResource("search", search.GetSearchResource())
 	}
 	return api
 }
